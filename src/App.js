f70c62acd658ef157586e6bf77c96b76ad1de90d
yunxinjuanfei <yunxinjuanfei@126.com>
2019-02-14 17:41:36 +0800

    auto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
     render() {
         return ( 
-           <div></div>
+        <div></div>
         );
     }
 }
